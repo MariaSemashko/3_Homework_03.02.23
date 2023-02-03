95e6684c7db7e598b4721031e9308215d92612d2
Maria <maria.a.semashko@gmail.com>
2023-02-03 08:49:51 +0300

    Task 3

---
+++ Task003/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не натуральное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+void GetCube(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write($"{i*i*i}, ");
+    }
+}
+
+int number = GetNumber("Введите натуральное число");
+GetCube(number);
